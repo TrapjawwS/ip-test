b4ff8f853a1ce539010040d37f40593605cab935
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2023-10-27 21:42:16 +0000

    yes

---
+++ script.js
@@ -0,0 +1,15 @@
+if (gfg == "12.38.208.90") {
+    <body>
+	<center>
+		<h1>Hi</h1>
+		<h3>Public IP Address of user is:</h3>
+		<p id="gfg"></p>
+	</center>
+</body>
+
+} else {
+    <body>
+        <p>Invalid access</p>
+    </body>
+}
+
